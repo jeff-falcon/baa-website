71ec3ad72fd2c041c2172816a979d0e113f06bd1
Jeff Falcon <jeff@igloo.net>
2024-01-31 15:12:52 -0500

    fix colliding project names

--- src/routes/artists/[slug]/[project]/+page.server.ts
+++ src/routes/artists/[slug]/[project]/+page.server.ts
@@ -2,11 +2,19 @@ import type { PageServerLoad } from './$types';
 import { getClient } from '$lib/sanity';
 import type { Artist, Project, ProjectMedia, ProjectMediaPair, ProjectMediaTrio } from '$lib/types';
 import { error } from '@sveltejs/kit';
-import { parseProjectSlug, parseCloudinaryImage, parseProjectMediaFromData, parseArtistFromData, mergePortfolioIntoProjects } from '$lib/parse';
+import {
+	parseProjectSlug,
+	parseCloudinaryImage,
+	parseProjectMediaFromData,
+	parseArtistFromData,
+	mergePortfolioIntoProjects
+} from '$lib/parse';
 
-export const load: PageServerLoad = async ({ params }): Promise<{ project?: Project, artist?: Artist }> => {
-  const client = getClient();
-  const groqArtist = `*[_type == "artist" && slug.current == "${params.slug}"]{
+export const load: PageServerLoad = async ({
+	params
+}): Promise<{ project?: Project; artist?: Artist }> => {
+	const client = getClient();
+	const groqArtist = `*[_type == "artist" && slug.current == "${params.slug}"]{
     ...,
     "portfolio": portfolio->{..., "slug": slug.current},
     "featured": null,
@@ -40,33 +48,37 @@ export const load: PageServerLoad = async ({ params }): Promise<{ project?: Proj
       },
     },
   }`;
-  let artistData = await client.fetch(groqArtist);
+	let artistData = await client.fetch(groqArtist);
 
-  if (!artistData?.[0]) throw error(404, 'Artist not found');
+	if (!artistData?.[0]) throw error(404, 'Artist not found');
 
-  artistData = artistData[0]
-  mergePortfolioIntoProjects(artistData)
+	artistData = artistData[0];
+	mergePortfolioIntoProjects(artistData);
 
-  const projects: string[] = artistData.projects?.flatMap((p: any) => {
-    if (p._type === 'project') {
-      return [parseProjectSlug(params.slug, p.slug), p.slug];
-    } else if (p._type === 'project_pair') {
-      return [parseProjectSlug(params.slug, p.left.slug), p.left.slug, parseProjectSlug(params.slug, p.right.slug), p.right.slug];
-    } else if (p._type === 'project_trio') {
-      return [parseProjectSlug(params.slug, p.top.slug), p.top.slug, parseProjectSlug(params.slug, p.bottom.slug), p.bottom.slug, parseProjectSlug(params.slug, p.side.slug), p.side.slug];
-    }
-    return []
-  }) ?? [];
+	const artistProjectsSlugs: string[] =
+		artistData.projects
+			?.flatMap((p: any) => {
+				if (p._type === 'project') {
+					return [p.slug];
+				} else if (p._type === 'project_pair') {
+					return [p.left?.slug, p.right?.slug];
+				} else if (p._type === 'project_trio') {
+					return [p.top?.slug, p.bottom?.slug, p.side?.slug];
+				}
+				return [];
+			})
+			.filter((slug: string) => Boolean(slug)) ?? [];
 
-  if (!projects.includes(params.project)) {
-    throw error(404, 'Project not found');
-  }
+	const foundProjectSlug = artistProjectsSlugs.find(
+		(slug) => slug === params.project || slug === `${params.slug}-${params.project}`
+	);
+	if (!foundProjectSlug) {
+		throw error(404, 'Project not found');
+	}
 
-  artistData.projects = []
+	artistData.projects = [];
 
-  const parsedProjectSlug = parseProjectSlug(params.slug, params.project);
-
-  const groqProject = `*[_type == "project" && (slug.current == "${parsedProjectSlug}" || slug.current == "${params.slug}-${parsedProjectSlug}")]{
+	const groqProject = `*[_type == "project" && slug.current == "${foundProjectSlug}"]{
 			...,
 			media[]{
 				_type == 'item' => @->,
@@ -85,76 +97,76 @@ export const load: PageServerLoad = async ({ params }): Promise<{ project?: Proj
 			},
 			"tags": tags[]->prefLabel
 		}`;
-  const data = await client.fetch(groqProject);
-  const projectData = data[0];
-  const mediaList: Array<ProjectMedia | ProjectMediaPair | ProjectMediaTrio> = []
-  if (projectData.media) {
-    for (const media of projectData.media) {
-      if (media._type === 'project_media') {
-        const item = parseProjectMediaFromData(media);
-        if (item) mediaList.push(item)
-      }
-      if (media._type === 'item_pair') {
-        const left = parseProjectMediaFromData(media.left, false)
-        const right = parseProjectMediaFromData(media.right, false)
-        if (!left && !right) {
-          continue;
-        }
-        if (!left || !right) {
-          const item = left ?? right;
-          if (item) mediaList.push(item)
-        } else {
-          mediaList.push(<ProjectMediaPair>{
-            _type: 'item_pair',
-            left,
-            right
-          });
-        }
-      }
-      if (media._type === 'item_trio') {
-        const top = parseProjectMediaFromData(media.top, false)
-        const bottom = parseProjectMediaFromData(media.bottom, false)
-        const side = parseProjectMediaFromData(media.side, false)
-        if (!top || !bottom || !side) {
-          continue;
-        }
-        mediaList.push(<ProjectMediaTrio>{
-          _type: 'item_trio',
-          top,
-          bottom,
-          side
-        });
-      }
-    }
-  }
+	const data = await client.fetch(groqProject);
+	const projectData = data[0];
+	const mediaList: Array<ProjectMedia | ProjectMediaPair | ProjectMediaTrio> = [];
+	if (projectData.media) {
+		for (const media of projectData.media) {
+			if (media._type === 'project_media') {
+				const item = parseProjectMediaFromData(media);
+				if (item) mediaList.push(item);
+			}
+			if (media._type === 'item_pair') {
+				const left = parseProjectMediaFromData(media.left, false);
+				const right = parseProjectMediaFromData(media.right, false);
+				if (!left && !right) {
+					continue;
+				}
+				if (!left || !right) {
+					const item = left ?? right;
+					if (item) mediaList.push(item);
+				} else {
+					mediaList.push(<ProjectMediaPair>{
+						_type: 'item_pair',
+						left,
+						right
+					});
+				}
+			}
+			if (media._type === 'item_trio') {
+				const top = parseProjectMediaFromData(media.top, false);
+				const bottom = parseProjectMediaFromData(media.bottom, false);
+				const side = parseProjectMediaFromData(media.side, false);
+				if (!top || !bottom || !side) {
+					continue;
+				}
+				mediaList.push(<ProjectMediaTrio>{
+					_type: 'item_trio',
+					top,
+					bottom,
+					side
+				});
+			}
+		}
+	}
 
-  const artist = parseArtistFromData(artistData)
+	const artist = parseArtistFromData(artistData);
 
-  const title = projectData.title || projectData.name;
+	const title = projectData.title || projectData.name;
 
-  const project: Project = {
-    _type: 'project',
-    _id: projectData._id,
-    type: projectData.type === 'portfolio' ? 'portfolio' : 'project',
-    pageTitle: `${title} | ${artist?.name ?? ''} | BAA Global`,
-    name: projectData.name,
-    title,
-    metaDescription: projectData.meta_description,
-    shortName: projectData.short_name || title || '',
-    slug: projectData.slug.current,
-    descriptionIntro: projectData.description_intro,
-    description: projectData.description,
-    client: projectData.client,
-    credits: projectData.credits ?? [],
-    image: parseCloudinaryImage(projectData.image, projectData.image_mobile),
-    videoBgSrc: projectData.thumb_vimeo_src,
-    videoBgSrcHd: projectData.thumb_vimeo_src_hd,
-    media: mediaList,
-    bgColor: projectData.bg_color?.value,
-    relatedProjects: [],
-    showRelatedProjects: false,
-    tags: projectData.tags ?? [],
-  };
+	const project: Project = {
+		_type: 'project',
+		_id: projectData._id,
+		type: projectData.type === 'portfolio' ? 'portfolio' : 'project',
+		pageTitle: `${title} | ${artist?.name ?? ''} | BAA Global`,
+		name: projectData.name,
+		title,
+		metaDescription: projectData.meta_description,
+		shortName: projectData.short_name || title || '',
+		slug: projectData.slug.current,
+		descriptionIntro: projectData.description_intro,
+		description: projectData.description,
+		client: projectData.client,
+		credits: projectData.credits ?? [],
+		image: parseCloudinaryImage(projectData.image, projectData.image_mobile),
+		videoBgSrc: projectData.thumb_vimeo_src,
+		videoBgSrcHd: projectData.thumb_vimeo_src_hd,
+		media: mediaList,
+		bgColor: projectData.bg_color?.value,
+		relatedProjects: [],
+		showRelatedProjects: false,
+		tags: projectData.tags ?? []
+	};
 
-  return { project, artist };
+	return { project, artist };
 };
